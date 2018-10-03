bbc486e82250a1e12e7fa0cb06b3b8525bdac1b1
endotnick <endotnick@gmail.com>
2018-10-02 18:32:36 -0700

    gulp initiated transpile working

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,14 @@ const lint = require('gulp-eslint');
 const webpack = require('webpack-stream');
 const webpackConfig = require('./webpack.config');
 const runSequence = require('run-sequence');
+const babel = require('gulp-babel');
+const babelify = require('babelify');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const sourcemaps = require('gulp-sourcemaps');
+const util = require('gulp-util');
+
 
 gulp.task('default', ['dev', 'watch']);
 
@@ -60,6 +68,51 @@ gulp.task('images', () =>
     ))
     .pipe(gulp.dest('build/img')));
 
+
+gulp.task('transbabel', () =>
+  gulp.src('src/js/*.js')
+    .pipe(babel({
+      presets: ['env'],
+    }))
+    .pipe(gulp.dest('build/js/')));
+
+gulp.task('browserify', () => {
+  const files = [
+    './src/js/main.js',
+    './src/js/restaurant_info.js',
+    './src/js/sw.js',
+  ];
+  files.map(entry =>
+    browserify({
+      entries: [entry],
+      debug: true,
+      transform: [babelify.configure({
+        presets: ['env'],
+      })],
+    })
+      .bundle()
+      .pipe(source(entry))
+      .pipe(gulp.dest('./build/js/')));
+});
+
+/*
+  const b = browserify({
+    entries: './src/js/main.js',
+    debug: true,
+    transform: [babelify.configure({
+      presets: ['env'],
+    })],
+  });
+  b.bundle()
+    .pipe(source('./src/js/main.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    // Add other gulp transformations (eg. uglify) to the pipeline here.
+    .on('error', util.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./build/js/'))
+*/
+
 gulp.task('lint', () =>
   gulp.src(['src/js/*.js'])
     .pipe(lint())
@@ -76,9 +129,14 @@ gulp.task('cp_sw', () =>
 gulp.task('compile', () =>
   runSequence('pack', 'cp_sw', 'del_compiledSW'));
 
+gulp.task('transpile', () =>
+  runSequence('transbabel', 'cp_sw', 'del_compiledSW'));
+
 gulp.task('dev', ['clean', 'lint', 'compile', 'images']);
 
 gulp.task('prod', ['clean', 'compile', 'images']);
+
+gulp.task('babel-test', ['clean', 'transpile', 'images']);
 
 gulp.task('watch', () =>
   gulp.watch('src/js/*.js', ['lint', 'compile']));
