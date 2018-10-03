ec66f21819af474f2a85b4cc54c93cbad30201fd
endotnick <endotnick@gmail.com>
2018-10-02 23:41:55 -0700

    update server

--- server.js
+++ server.js
@@ -5,16 +5,9 @@ const file = new server.Server('./');
 const port = process.env.PORT || 5000;
 
 http.createServer((request, response) => {
-  response.writeHead(200, {
-    'Content-Type': 'text/plain',
-  });
-  response.write('hello heroku!', 'utf-8');
-  response.end();
-  /*
   request.addListener('end', () => {
     file.serve(request, response);
   }).resume();
-  */
 }).listen(port, () => {
   console.log(`app up on port: ${port}`);
 });
